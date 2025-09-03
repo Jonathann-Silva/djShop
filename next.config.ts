8d7b1276a0dc975646777636e77f7df9c103b4c0
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-09-03 19:52:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'horizonplay.fbitsstatic.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
