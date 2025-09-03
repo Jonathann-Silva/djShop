6e3f57ba0db547778c9fe1649ae22cd258885171
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-09-03 19:51:32 +0000

    Error: Invalid src prop (https://i.zst.com.br/thumbs/12/3c/1c/-111529754

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.zst.com.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
