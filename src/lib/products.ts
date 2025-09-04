0e39215ade5dd3d4daeeff86477b51c357db32c6
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-09-04 20:12:17 +0000

    O apple Iphone 16 128gb esta zero no site, porem ele ainda esta mostrand

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -44,7 +44,7 @@ export const PRODUCTS: Product[] = [
       "verde",
       "vermelho"
     ],
-    "scrapingUrl": "https://www.lgimportados.com/produto/apple-iphone-16-128gb-a-3287-black-hn-chip-codigo-781800",
+    "scrapingUrl": "",
     "priceMargin": 0
   },
   {
