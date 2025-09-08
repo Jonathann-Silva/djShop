f7b83ca2ee803621ae63dfd9bea26d5bd86265c8
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-09-08 00:00:13 +0000

    Colocar um icone rodando (carregando), enquanto a pagina esta sendo carr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <AuthProvider>
           <CartProvider>
             <Header />
-            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col">
               {children}
             </main>
             <Footer />
