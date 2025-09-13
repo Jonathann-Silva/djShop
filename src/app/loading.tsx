88c4257ac34d7cb3b38e8acc0c8cb4088bff9e8a
Jonathann-Silva <168100734+Jonathann-Silva@users.noreply.github.com>
2025-09-12 21:31:49 -0300

    removendo o projeto

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-export default function Loading() {
-  return (
-    <div className="flex flex-1 items-center justify-center">
-      <Loader2 className="h-16 w-16 animate-spin text-primary" />
-    </div>
-  );
-}
