5b45c03dadbc597d878569868183ff7fd20f0825
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-09-17 23:55:23 +0000

    Atualiza @grpc/grpc-js e corrige vulnerabilidades

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,14 +1,17 @@
+// src/lib/firebase-admin.ts
+
 import admin from 'firebase-admin';
-import { cert } from 'firebase-admin/app';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID!,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!,
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   });
 }
 
-export const adminDb = admin.firestore();
+const adminDb = admin.firestore();
+
+export { adminDb };
