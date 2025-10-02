cec629f7e0c9ba24d09335dbb012a3c5f43cdefc
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-10-02 20:19:02 +0000

    AS IMAGENS ESTAO SENDO PUXADAS DIRETO DO SITE, OU ESTA SENDO SALVA DENTR

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -19,6 +19,7 @@ export interface Perfume {
   price?: number | null;
   costPrice?: number | null;
   createdAt?: number;
+  category?: 'perfume';
 }
 
 export interface Eletronico {
@@ -34,6 +35,7 @@ export interface Eletronico {
   price?: number | null;
   costPrice?: number | null;
   createdAt?: number;
+  category?: 'eletronico';
 }
 
 export interface Bebida {
@@ -49,6 +51,7 @@ export interface Bebida {
   price?: number | null;
   costPrice?: number | null;
   createdAt?: number;
+  category?: 'bebida';
 }
 
 
